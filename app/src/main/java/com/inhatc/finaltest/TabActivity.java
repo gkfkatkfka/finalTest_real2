e3f21511aa13a598ee72cee79eb9b7209f9f9c52
gkfkatkfka <rkdgkfkaqkqh@naver.com>
2021-06-20 04:08:46 +0900

    bookFragment 하기전 commit

--- app/src/main/java/com/inhatc/finaltest/TabActivity.java
+++ app/src/main/java/com/inhatc/finaltest/TabActivity.java
@@ -6,6 +6,7 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.material.tabs.TabLayout;
@@ -44,4 +45,6 @@ public class TabActivity extends AppCompatActivity{
 
         return email;
     }
+
+
 }
